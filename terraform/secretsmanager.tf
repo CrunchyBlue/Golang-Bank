1b99c4d130d7f9072c7cccbc2e1d171493bd366d
David <51713101+CrunchyBlue@users.noreply.github.com>
2023-05-12 12:49:19 -0500

    Update db source secret

--- terraform/secretsmanager.tf
+++ terraform/secretsmanager.tf
@@ -20,7 +20,7 @@ resource "aws_secretsmanager_secret_version" "secret_val" {
   secret_id     = aws_secretsmanager_secret.secret.id
   secret_string = jsonencode({
     "DB_DRIVER" : "postgres",
-    "DB_SOURCE" : "postgresql://${aws_db_instance.db.username}:${jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["password"]}@${aws_db_instance.db.endpoint}:${aws_db_instance.db.port}",
+    "DB_SOURCE" : "'postgresql://${aws_db_instance.db.username}:${jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["password"]}@${aws_db_instance.db.endpoint}:${aws_db_instance.db.port}'",
     "SERVER_ADDRESS" : "0.0.0.0:8080",
     "ACCESS_TOKEN_SYMMETRIC_KEY" : random_password.symmetric_key.result,
     "ACCESS_TOKEN_DURATION" : "15m"
